d03aa8de65e2203d1703290d11c67a4a871490e2
jaspreet <jaspreet605@gmail.com>
2019-06-10 17:08:12 +0530

    Login Service Created.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UrlSerializer, Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
@@ -13,18 +15,19 @@ export class LoginComponent implements OnInit {
 
   userObj = new User();
 
-  constructor(private router: Router) { 
+  constructor(private router: Router, private loginService : LoginService) { 
+
   }
 
   ngOnInit() {
   }
 
   login(){
-    this.clickmessage = "Login Clicked.";
-    this.router.navigateByUrl("/home")   ; 
+ //   this.clickmessage = "Login Clicked.";
+   // this.router.navigateByUrl("/home")   ; 
    // alert(JSON.stringify(this.userObj));
 
-   
+   this.loginService.callHTTPService();
 
   }
 
---
+++ src/app/service/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { 
+    
+  }
+  callHTTPService() {
+   
+    console.log("Correct Service");
+
+  }
+
+ 
+}
