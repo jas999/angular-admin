c739b11de27c1b798e21030852bfc7820ee97f89
jaspreet <jaspreet605@gmail.com>
2019-06-18 17:47:52 +0530

    APIURL

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -6,8 +6,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UserService {
 
+  private apiURL :  String  = "http://ec2-13-59-161-93.us-east-2.compute.amazonaws.com:8080/cms-v1/cms/user/all";
+
   constructor(private http: HttpClient) { }
 
+  public getCustomers(){
 
+    return this.http.get<Object[]>(`${this.apiURL}/customers`);
+
+  }
   
 }
